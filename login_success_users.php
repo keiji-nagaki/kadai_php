fc00cd94c752a1c50e03845e5a45bc351b8faf72
長城恵二 <n.keiji0421@gmail.com>
2022-12-16 08:28:42 +0900

    ログイン機能追加

---
+++ login_success_users.php
@@ -0,0 +1,31 @@
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>ログイン成功</title>
+  <link rel="stylesheet" href="css/test.css" type="text/css">
+ </head>
+ <body>
+
+
+  <form>
+
+    <div>
+      <a href="estimate_page.html" class="estimae_btn">見積・購入</a>
+    </div>
+    <div>
+      <a href="valvelist_page.html" class="valvelist_btn">バルブリスト</a>
+    </div>
+    <div>
+      <a href="inquiry_page.html" class="inquiry_btn">お問合せ</a>
+    </div>
+    <div>
+      <a href="todo_txt_input.php" class="inquiry_btn">アンケート</a>
+    </div>
+
+
+
+
+  </form>
+</body>
+ </body>
+</html> 
